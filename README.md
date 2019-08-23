900c1a7a064ebf19c6b4fb23e58c7ab22606cb63
Nikola Tasic <ntasic7@gmail.com>
2019-08-24 01:27:53 +0200

    initial, everything works i guess

---
+++ README.md
@@ -0,0 +1,31 @@
+# Boat
+
+## Description
+
+Small `electron` based borderless floating browser inspired by [pennywise](https://github.com/kamranahmedse/pennywise).
+
+Why is it named boat? Well boats float... Most of the time at least.
+
+## Usage 
+
+`$ boat`
+
+Opens a blank page with address bar.
+
+`$ boat --url=https://youtube.com/tv` 
+
+Opens `https://youtube.com/tv` in a borderless window.
+
+
+| **Shortcut**                       | **Description**                  |
+|------------------------------------|----------------------------------|
+| <kbd>F5</kbd>                      | Refresh page                     |
+| <kbd>Cmd/Ctrl + L</kbd>            | Open address bar                 |
+| <kbd>Cmd/Ctrl + Alt + Up</kbd>     | Increase opacity                 |
+| <kbd>Cmd/Ctrl + Alt + Down</kbd>   | Decrease opacity                 |
+| <kbd>Cmd/Ctrl + Shift + I</kbd>    | Show Developer Tools             |
+| <kbd>Cmd/Ctrl + Shift + A</kbd>    | Toggle always on top             |
+| <kbd>Alt + Left</kbd>              | Go back                          |
+| <kbd>Alt + Right</kbd>             | Go forwards                      |
+
+There is not much to it. It just offers a convenient sleek window for multitasking.
---
+++ electron.js
@@ -0,0 +1,136 @@
+const {app, BrowserWindow, ipcMain, globalShortcut} = require("electron");
+
+const argv = require("yargs").argv;
+
+let mainWindow = null;
+let urlWindow = null;
+
+async function main() {
+	mainWindow = new BrowserWindow({
+		center: true,
+		darkTheme: true,
+		autoHideMenuBar: true,
+		transparent: true,
+		opacity: 0.5,
+		title: "Boat",
+		backgroundColor: "black",
+		width: 1280,
+		height: 720,
+		webPreferences: {nodeIntegration: true},
+	});
+
+	globalShortcut.register("CmdOrCtrl+Alt+Up", () => {
+		const opacity = mainWindow.getOpacity();
+		const newOpacity = opacity + .05;
+		if (newOpacity <= 1) {
+			mainWindow && mainWindow.setOpacity(newOpacity);
+		}
+	});
+	globalShortcut.register("CmdOrCtrl+Alt+Down", () => {
+		const opacity = mainWindow.getOpacity();
+		const newOpacity = opacity - .05;
+		if (newOpacity >= 0) {
+			mainWindow && mainWindow.setOpacity(newOpacity);
+		}
+	});
+	globalShortcut.register("CmdOrCtrl+Shift+A", () => {
+		mainWindow && mainWindow.setAlwaysOnTop(!mainWindow.isAlwaysOnTop());
+	});
+	globalShortcut.register("CmdOrCtrl+Shift+I", () => {
+		BrowserWindow.getFocusedWindow().webContents.openDevTools();
+	});
+	globalShortcut.register("F5", () => {
+		mainWindow && mainWindow.webContents.reload();
+	});
+	globalShortcut.register("Alt+Left", () => {
+		mainWindow && mainWindow.webContents.canGoBack() && mainWindow.webContents.goBack();
+	});
+	globalShortcut.register("Alt+Right", () => {
+		mainWindow && mainWindow.webContents.canGoForward() && mainWindow.webContents.goForward();
+	});
+	globalShortcut.register("Escape", () => {
+		urlWindow && urlWindow.close();
+	});
+	globalShortcut.register("CmdOrCtrl+L", () => {
+		!urlWindow && openUrlWindow();
+	});
+	mainWindow.on("move", () => {
+		if (urlWindow) {
+			urlWindow.setPosition(mainWindow.getBounds().x, mainWindow.getBounds().y);
+			urlWindow.focus();
+		}
+	});
+	mainWindow.on("resize", () => {
+		if (urlWindow) {
+			urlWindow.setSize(mainWindow.getBounds().width, 50);
+		}
+	});
+	mainWindow.on("closed", app.exit);
+
+	try {
+		await mainWindow.loadURL(argv.url || argv._[0] || "");
+	} catch (e) {
+		mainWindow && await openUrlWindow();
+	}
+	mainWindow.show();
+	mainWindow.setMenu(null);
+}
+
+ipcMain.on("page-ctl", (event, args) => {
+	if (mainWindow) {
+		switch (args.action) {
+			case "backward":
+				mainWindow.webContents.canGoBack() && mainWindow.webContents.goBack();
+				break;
+			case "forward":
+				mainWindow.webContents.canGoForward() && mainWindow.webContents.goForward();
+				break;
+			case "reload":
+				mainWindow.webContents.reload();
+				break;
+		}
+	}
+});
+
+
+ipcMain.on("url-open", async (event, args) => {
+	if (mainWindow) {
+		const url = require("url");
+		const openUrl = url.format({
+			protocol: "http",
+			slashes: true,
+			pathname: args.url,
+		});
+		console.log("opening: ", openUrl);
+		await mainWindow.loadURL(openUrl);
+		urlWindow && urlWindow.close();
+	}
+});
+
+async function openUrlWindow() {
+	const bounds = mainWindow.getBounds();
+	urlWindow = new BrowserWindow({
+		height: 50,
+		width: bounds.width,
+		x: bounds.x,
+		y: bounds.y,
+		// resizable: false,
+		movable: false,
+		alwaysOnTop: true,
+		autoHideMenuBar: true,
+		titleBarStyle: "hidden",
+		frame: false,
+		webPreferences: {nodeIntegration: true},
+	});
+	await urlWindow.loadFile("views/urlWindow.html");
+	urlWindow.on("ready-to-show", urlWindow.show);
+	urlWindow.on("closed", () => urlWindow = null);
+	urlWindow.on("blur", () => {
+		urlWindow.close();
+		urlWindow = null;
+	});
+}
+
+
+app.on("ready", main);
+app.on("window-all-closed", app.exit);
